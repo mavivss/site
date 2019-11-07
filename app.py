7144ee3266a94a554d282c1e95940b998ca811ec
Emanuela <emanuela_icm@hotmail.com>
2019-11-07 15:32:07 -0300

    adicionando banco de dados e tabela de mensagens

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
-
+from Flask _sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config ['SQLALCHEMY DATABASE_URI:'] = 'sqlite:///minhasmensagens.db'
+
+class Mensagem(db.model):
+    id = db.column(db.interger, primary key=True)
+    mensagem = db.Column(db.text, nullable=False)
+
+    def__repre__(self)
+    return self.mensagem
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+@app.route('/nova mensagem')
+def novaMensagem():
+    return render_template('mensagem.html')
 
 
 if __name__ == "__main__":
